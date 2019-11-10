7e04e702abcb50cda7a11b52f1f448aa8e3f3a2b
Bill Perry <wlperry@gmail.com>
2019-11-09 19:31:22 -0600

    begining of first script

--- scripts/global change script.R
+++ scripts/global change script.R
@@ -30,7 +30,7 @@ loa_co2.df <- read_excel("data/climate_change_module_dataset.xls",
 
 # Plot of global tempreatures ------
 global_temp.plot <- global_temp.df %>%
-                          ggplot(aes(year, air_temp_c)) +
+                          ggplot(aes(year, temp_c)) +
                           geom_line()+
                           geom_point()
 global_temp.plot
@@ -38,18 +38,41 @@ ggplotly(global_temp.plot)
 
 # Plot of global tempreatures - subset -----
 # Enter the years here
-min_year = 1976
-max_year =  1998
+min_year_global = 1976
+max_year_global =  1998
 
 global_temp.plot <- global_temp.df %>%
-  filter(year >= min_year & year <= max_year) %>%
-  ggplot(aes(year, air_temp_c)) +
+  filter(year >= min_year_global & year <= max_year_global) %>%
+  ggplot(aes(year, temp_c)) +
   geom_line()+
   geom_point()
 global_temp.plot
 ggplotly(global_temp.plot)
 
 
+# Plot of Vostok tempreatures ------
+vostok_temp.plot <- vostok_temp.df %>%
+  ggplot(aes(ice_age_year_bp, temp_c)) +
+  geom_line()+
+  geom_point()
+vostok_temp.plot
+ggplotly(vostok_temp.plot)
+
+# Plot of Vostok tempreatures - subset -----
+# Enter the years here
+min_year_vostok = 231979
+max_year_vostok =  237643
+# run this to see the number of years
+max_year_vostok - min_year_vostok  
+
+# plot of temp over time for sub area
+vostok_temp.plot <- vostok_temp.df %>%
+  filter(ice_age_year_bp >= min_year_vostok & ice_age_year_bp <= max_year_vostok) %>%
+  ggplot(aes(ice_age_year_bp, temp_c)) +
+  geom_line()+
+  geom_point()
+vostok_temp.plot
+ggplotly(vostok_temp.plot)
 
 
                         
